d5ef056627ec8f827679d284507531c0560af8ae
aomilkmoney <theadamoliveras@gmail.com>
2021-03-25 18:29:46 -0700

    Base build of the scheduler component.

---
+++ pages/index.vue
@@ -0,0 +1,139 @@
+<template>
+  <v-container>
+    <v-row no-gutters>
+      <v-col cols="4">
+        <v-card style="height: 76px">
+          <v-card-text> Inventory Item </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col v-for="week in weeks" :key="week.throughDates" cols="1">
+        <v-card>
+          <v-card-text>3/22 - 3/28 </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col cols="4">
+        <v-list style="padding: 0px">
+          <v-list-item v-for="(item, i) in items" :key="i" style="padding: 0px">
+            <v-card style="width: 100%; height: 83px" class="justify-center">
+              <v-card-text class="text-center">
+                <v-list-item-content>
+                  <v-list-item-title
+                    class="pb-2"
+                    v-text="item.title"
+                  ></v-list-item-title>
+                </v-list-item-content>
+              </v-card-text>
+            </v-card>
+          </v-list-item>
+        </v-list>
+      </v-col>
+      <v-col cols="8">
+        <v-row
+          v-for="appointment in appointments"
+          :key="appointment.id"
+          no-gutters
+        >
+          <v-col cols="">
+            <v-card style="height: 83px">
+              <v-btn
+                class="mt-7"
+                small
+                rounded
+                color="primary"
+                :style="appointment.marginLeft + appointment.width"
+                >Lamar</v-btn
+              >
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      weeks: [
+        { throughDates: '3/22-3/29' },
+        { throughDates: '3/22-3/29' },
+        { throughDates: '3/22-3/29' },
+        { throughDates: '3/22-3/29' },
+        { throughDates: '3/22-3/29' },
+        { throughDates: '3/22-3/29' },
+        { throughDates: '3/22-3/29' },
+        { throughDates: '3/22-3/29' },
+      ],
+      items: [
+        { title: '35th and Boston' },
+        { title: 'Pacific and Redfield' },
+        { title: 'Pacific and Redfield' },
+        { title: 'Pacific and Redfield' },
+        { title: 'Pacific and Redfield' },
+        { title: 'Pacific and Redfield' },
+        { title: 'Pacific and Redfield' },
+        { title: 'Pacific and Redfield' },
+        { title: 'Pacific and Redfield' },
+        { title: 'Pacific and Redfield' },
+        { title: 'Pacific and Redfield' },
+        { title: 'Pacific and Redfield' },
+      ],
+      appointments: [
+        {
+          id: 'adsafa',
+          vendorName: 'Lamar',
+          width: 'width: 50%',
+          marginLeft: 'margin-left: 100px;',
+        },
+        {
+          id: 'sadgat',
+          vendorName: 'Lamar',
+          width: 'width: 20%',
+          marginLeft: 'margin-left: 150px;',
+        },
+        {
+          id: 'fgsdftr',
+          vendorName: 'Lamar',
+          width: 'width: 50%',
+          marginLeft: 'margin-left: 270px;',
+        },
+        {
+          id: '3452t',
+          vendorName: 'Lamar',
+          width: 'width: 20%',
+          marginLeft: 'margin-left: 200px;',
+        },
+        {
+          id: 'scvdsga',
+          vendorName: 'Lamar',
+          width: 'width: 50%',
+          marginLeft: 'margin-left: 50px;',
+        },
+        {
+          id: 'adsdrtafa',
+          vendorName: 'Lamar',
+          width: 'width: 70%',
+          marginLeft: 'margin-left: 150px;',
+        },
+        {
+          id: 'ads343afa',
+          vendorName: 'Lamar',
+          width: 'width: 50%',
+          marginLeft: 'margin-left: 210px;',
+        },
+        {
+          id: 'adsagrsedazfa',
+          vendorName: 'Lamar',
+          width: 'width: 20%',
+          marginLeft: 'margin-left: 230px;',
+        },
+      ],
+    }
+  },
+}
+</script>
---
+++ pages/inspire.vue
@@ -0,0 +1,248 @@
+<template>
+  <div>
+    <p>
+      {{ fname }}
+    </p>
+
+    <v-data-table
+      v-model="selected"
+      :headers="headers"
+      :items="items"
+      class="elevation-1"
+      @click:row="showAlert"
+    >
+      <template slot="items" slot-scope="props">
+        <tr>
+          <td>{{ props.item.name }}</td>
+          <td class="text-xs-right">{{ props.item.calories }}</td>
+          <td class="text-xs-right">{{ props.item.fat }}</td>
+          <td class="text-xs-right">{{ props.item.carbs }}</td>
+          <td class="text-xs-right">{{ props.item.protein }}</td>
+          <td class="text-xs-right">{{ props.item.sodium }}</td>
+          <td class="text-xs-right">{{ props.item.calcium }}</td>
+        </tr>
+      </template>
+    </v-data-table>
+    <v-btn slot="activator" small fab color="success" @click="dialog = true">
+      <v-icon>edit</v-icon>
+    </v-btn>
+
+    <v-layout row justify-center>
+      <v-dialog v-model="dialog" persistent max-width="600px" absolute>
+        <v-card>
+          <v-card-title>
+            <span class="headline">{{ fname }} - Registration</span>
+            <br />
+          </v-card-title>
+          <v-card-text>
+            <v-container grid-list-md>
+              <v-layout wrap>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field
+                    label="Legal first name*"
+                    required
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field
+                    label="Legal middle name"
+                    hint="example of helper text only on focus"
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field
+                    label="Legal last name*"
+                    hint="example of persistent helper text"
+                    persistent-hint
+                    required
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-text-field label="Email*" required></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-text-field
+                    label="Password*"
+                    type="password"
+                    required
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6>
+                  <v-select
+                    :items="['0-17', '18-29', '30-54', '54+']"
+                    label="Age*"
+                    required
+                  ></v-select>
+                </v-flex>
+                <v-flex xs12 sm6>
+                  <v-autocomplete
+                    :items="[
+                      'Skiing',
+                      'Ice hockey',
+                      'Soccer',
+                      'Basketball',
+                      'Hockey',
+                      'Reading',
+                      'Writing',
+                      'Coding',
+                      'Basejump',
+                    ]"
+                    label="Interests"
+                    multiple
+                  ></v-autocomplete>
+                </v-flex>
+              </v-layout>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="dialog = false"
+              >Close</v-btn
+            >
+            <v-btn color="blue darken-1" text @click="dialog = false"
+              >Save</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-layout>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      fname: 'Test',
+      selected: [],
+      dialog: false,
+      test: true,
+      headers: [
+        {
+          text: 'Training Title',
+          align: 'left',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Mentor', value: 'calories', sortable: false },
+        { text: 'Schedule', value: 'fat', sortable: false },
+        {
+          text: '# of Registered Participants',
+          value: 'carbs',
+          sortable: false,
+        },
+        { text: 'Register', value: 'protein', sortable: false },
+        /* { text: 'Iron (%)', value: 'iron',sortable: false } */
+      ],
+      items: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 'Johnvel Mike Amarisca',
+          fat: 'July 8',
+          carbs: 3,
+          protein: 4.0,
+          iron: '1%',
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%',
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%',
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%',
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: '16%',
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: '0%',
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: '2%',
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: '45%',
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: '22%',
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: '6%',
+        },
+      ],
+    }
+  },
+  methods: {
+    highlightClickedRow(event) {
+      const tr = event.target.parentNode
+      tr.classList.add('highlight')
+    },
+    showAlert(a) {
+      // if (event.target.classList.contains('btn__content')) return
+      // alert('Alert! \n' + a.name)
+      // this.highlightClickedRow(a)
+
+      this.selected.name = a.name
+      this.selected.cal = a.calories
+      this.selected.fat = a.fat
+      this.fname = a.name
+      console.log(a)
+
+      console.log(this.selected.fat)
+      console.log(this.fname)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.highlight {
+  background-color: #fff;
+}
+</style>
