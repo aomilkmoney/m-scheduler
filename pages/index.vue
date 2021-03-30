4c7e2c05c7067134f4f659e0a8983f80003df565
aomilkmoney <theadamoliveras@gmail.com>
2021-03-29 19:24:57 -0700

    Latest working code.

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,37 @@
 <template>
-  <v-container>
+  <v-container style="max-height: 800px; overflow-y: scroll; min-width: 800px">
     <v-row no-gutters>
-      <v-col cols="4">
-        <v-card style="height: 76px">
-          <v-card-text> Inventory Item </v-card-text>
-        </v-card>
-      </v-col>
-
-      <v-col v-for="week in weeks" :key="week.throughDates" cols="1">
-        <v-card style="height: 76px">
-          <v-card-text>3/22 - 3/28 </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row no-gutters>
-      <v-col cols="4">
+      <v-row
+        no-gutters
+        style="
+          position: fixed;
+          overflow-x: scroll;
+          min-width: 800px;
+          height: 83px;
+          z-index: 2;
+        "
+      >
+        <v-col cols="4">
+          <v-card style="height: 83px">
+            <v-card-text> Inventory Item </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="8">
+          <v-row no-gutters>
+            <v-col
+              v-for="week in weeks"
+              :key="week.throughDates"
+              style="min-width: 50px"
+              cols="1"
+            >
+              <v-card style="height: 83px">
+                <v-card-text>3/22 - 3/28 </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-col cols="4" style="padding-top: 86px">
         <v-list style="padding: 0px">
           <v-list-item v-for="(item, i) in items" :key="i" style="padding: 0px">
             <v-card style="width: 100%; height: 83px" class="justify-center">
@@ -30,31 +47,25 @@
           </v-list-item>
         </v-list>
       </v-col>
+
       <v-col cols="8">
-        <v-row
-          v-for="appointment in appointments"
-          :key="appointment.id"
-          no-gutters
-        >
+        <v-row no-gutters style="min-width: 900px; padding-top: 86px"> </v-row>
+        <v-row v-for="item in items" :key="item.title" no-gutters>
           <v-col cols="">
-            <v-card style="height: 83px">
+            <div style="height: 83px; min-width: 900px; position: relative">
               <v-btn
+                v-for="appointment in item.appointments"
+                :key="appointment.id"
                 class="mt-7"
                 small
                 rounded
-                color="primary"
-                :style="appointment.marginLeft + appointment.width"
-                >Lamar</v-btn
-              >
-              <v-btn
-                class="mt-7"
-                small
-                rounded
-                color="primary"
-                :style="appointment.marginLeft + appointment.width"
-                >Lamar</v-btn
-              >
-            </v-card>
+                color="error"
+                :style="appointment.leftAbsolute + appointment.width"
+                style="z-index: 1"
+                >{{ appointment.clientName }}
+              </v-btn>
+              <div v-for="n in 11" :key="n" :class="'vl' + n"></div>
+            </div>
           </v-col>
         </v-row>
       </v-col>
@@ -76,72 +87,349 @@ export default {
         { throughDates: '3/22-3/29' },
         { throughDates: '3/22-3/29' },
         { throughDates: '3/22-3/29' },
+        { throughDates: '3/22-3/29' },
+        { throughDates: '3/22-3/29' },
+        { throughDates: '3/22-3/29' },
+        { throughDates: '3/22-3/29' },
       ],
       items: [
-        { title: '35th and Boston' },
-        { title: 'Pacific and Redfield' },
-        { title: 'Pacific and Redfield' },
-        { title: 'Pacific and Redfield' },
-        { title: 'Pacific and Redfield' },
-        { title: 'Pacific and Redfield' },
-        { title: 'Pacific and Redfield' },
-        { title: 'Pacific and Redfield' },
-        { title: 'Pacific and Redfield' },
-        { title: 'Pacific and Redfield' },
-        { title: 'Pacific and Redfield' },
-        { title: 'Pacific and Redfield' },
-      ],
-      appointments: [
         {
-          id: 'adsafa',
-          vendorName: 'Lamar',
-          width: 'width: 50%',
-          marginLeft: 'margin-left: 100px;',
+          title: '35th and Boston',
+          appointments: [
+            {
+              id: 'adsafa',
+              clientName: 'Naz X',
+              width: 'width: 142px',
+              leftAbsolute: 'left: 4px;',
+            },
+            {
+              id: 'sadgat',
+              clientName: 'Lil beeb',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 236px;',
+            },
+          ],
         },
         {
-          id: 'sadgat',
-          vendorName: 'Lamar',
-          width: 'width: 20%',
-          marginLeft: 'margin-left: 150px;',
+          title: 'Pacific and Redfield',
+          appointments: [
+            {
+              id: 'fgsdftr',
+              clientName: 'Rhianna',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 79px;',
+            },
+            {
+              id: '3452t',
+              clientName: 'Jay-Z',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 236px;',
+            },
+          ],
         },
         {
-          id: 'fgsdftr',
-          vendorName: 'Lamar',
-          width: 'width: 50%',
-          marginLeft: 'margin-left: 270px;',
+          title: 'Pacific and Redfield',
+          appointments: [
+            {
+              id: 'fgsdftr',
+              clientName: 'Kanye',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+            {
+              id: '3452t',
+              clientName: 'Kanye Again',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 462px;',
+            },
+          ],
         },
         {
-          id: '3452t',
-          vendorName: 'Lamar',
-          width: 'width: 20%',
-          marginLeft: 'margin-left: 200px;',
+          title: 'Pacific and Redfield',
+          appointments: [
+            {
+              id: 'fgsdftr',
+              clientName: 'Harrison Ford',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+            {
+              id: '3452t',
+              clientName: 'Jethro Tull',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+          ],
         },
         {
-          id: 'scvdsga',
-          vendorName: 'Lamar',
-          width: 'width: 50%',
-          marginLeft: 'margin-left: 50px;',
+          title: 'Pacific and Redfield',
+          appointments: [
+            {
+              id: 'fgsdftr',
+              clientName: 'Buford Tannen',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+            {
+              id: '3452t',
+              clientName: 'Amos McFly',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+          ],
         },
         {
-          id: 'adsdrtafa',
-          vendorName: 'Lamar',
-          width: 'width: 70%',
-          marginLeft: 'margin-left: 150px;',
+          title: 'Pacific and Redfield',
+          appointments: [
+            {
+              id: 'fgsdftr',
+              clientName: 'Dr. Emmet Brown',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+            {
+              id: '3452t',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+          ],
         },
         {
-          id: 'ads343afa',
-          vendorName: 'Lamar',
-          width: 'width: 50%',
-          marginLeft: 'margin-left: 210px;',
+          title: 'Pacific and Redfield',
+          appointments: [
+            {
+              id: 'fgsdftr',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+            {
+              id: '3452t',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+          ],
         },
         {
-          id: 'adsagrsedazfa',
-          vendorName: 'Lamar',
-          width: 'width: 20%',
-          marginLeft: 'margin-left: 30px;',
+          title: 'Pacific and Redfield',
+          appointments: [
+            {
+              id: 'fgsdftr',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+            {
+              id: '3452t',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+          ],
+        },
+        {
+          title: 'Pacific and Redfield',
+          appointments: [
+            {
+              id: 'fgsdftr',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+            {
+              id: '3452t',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+          ],
+        },
+        {
+          title: 'Pacific and Redfield',
+          appointments: [
+            {
+              id: 'fgsdftr',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+            {
+              id: '3452t',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+          ],
+        },
+        {
+          title: 'Pacific and Redfield',
+          appointments: [
+            {
+              id: 'fgsdftr',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+            {
+              id: '3452t',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+          ],
+        },
+        {
+          title: 'Pacific and Redfield',
+          appointments: [
+            {
+              id: 'fgsdftr',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+            {
+              id: '3452t',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+          ],
+        },
+        {
+          title: 'Pacific and Redfield',
+          appointments: [
+            {
+              id: 'fgsdftr',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+            {
+              id: '3452t',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+          ],
+        },
+        {
+          title: 'Pacific and Redfield',
+          appointments: [
+            {
+              id: 'fgsdftr',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+            {
+              id: '3452t',
+              clientName: 'Lamar',
+              width: 'width: 217px',
+              leftAbsolute: 'left: 232px;',
+            },
+          ],
         },
       ],
     }
   },
 }
 </script>
+
+<style>
+.sticky-row {
+  display: block;
+  position: fixed;
+  min-width: 800px;
+  height: 76px;
+}
+.vl1 {
+  border-left: 1px dotted lightgrey;
+  height: 81px;
+  position: absolute;
+  left: 74px;
+  top: 0;
+  z-index: 0;
+}
+.vl2 {
+  border-left: 1px dotted lightgrey;
+  height: 81px;
+  position: absolute;
+  left: 149px;
+  top: 0;
+  z-index: 0;
+}
+.vl3 {
+  border-left: 1px dotted lightgrey;
+  height: 81px;
+  position: absolute;
+  left: 224px;
+  top: 0;
+  z-index: 0;
+}
+.vl4 {
+  border-left: 1px dotted lightgrey;
+  height: 81px;
+  position: absolute;
+  left: 299px;
+  top: 0;
+  z-index: 0;
+}
+.vl5 {
+  border-left: 1px dotted lightgrey;
+  height: 81px;
+  position: absolute;
+  left: 374px;
+  top: 0;
+  z-index: 0;
+}
+.vl6 {
+  border-left: 1px dotted lightgrey;
+  height: 81px;
+  position: absolute;
+  left: 449px;
+  top: 0;
+  z-index: 0;
+}
+.vl7 {
+  border-left: 1px dotted lightgrey;
+  height: 81px;
+  position: absolute;
+  left: 524px;
+  top: 0;
+  z-index: 0;
+}
+.vl8 {
+  border-left: 1px dotted lightgrey;
+  height: 81px;
+  position: absolute;
+  left: 599px;
+  top: 0;
+  z-index: 0;
+}
+.vl9 {
+  border-left: 1px dotted lightgrey;
+  height: 81px;
+  position: absolute;
+  left: 674px;
+  top: 0;
+  z-index: 0;
+}
+.vl10 {
+  border-left: 1px dotted lightgrey;
+  height: 81px;
+  position: absolute;
+  left: 749px;
+  top: 0;
+  z-index: 0;
+}
+.vl11 {
+  border-left: 1px dotted lightgrey;
+  height: 81px;
+  position: absolute;
+  left: 824px;
+  top: 0;
+  z-index: 0;
+}
+</style>
